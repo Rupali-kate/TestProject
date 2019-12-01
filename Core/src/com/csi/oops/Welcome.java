9ce18731bc16411b9d1e03745fa82575bd26e9e4
Admin <Admin@Admin-PC>
2019-12-01 13:09:45 +0530

    Hello return commit push

---
+++ Core/src/com/csi/oops/Welcome.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.csi.oops;
+
+/**
+ * @author Admin
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO CSI");
+		System.out.println("Hello");
+
+	}
+
+}
